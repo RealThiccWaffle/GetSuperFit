0cf9045d6c3afdaaf2e64a1360522c7f677301cf
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-15 15:38:36 -0500

    t

--- GetSuperFitHome.py
+++ GetSuperFitHome.py
@@ -127,30 +127,25 @@ core_Endurance_Advanced = ["a", "b", "c", "d", "a", "b", "c", "d"]
 core_Endurance_Advanced2 = ["a", "b", "c", "d", "a", "b", "c", "d"]
 #-----------------SideBar---------------------------------------
 user_name = "no"
-user_login_True = False
 with st.sidebar:
     selected = option_menu(
         menu_title= "Main Menu",
         options= ["Home", "Chest Day", "Back/Shoulder Day", "Leg Day", "Arm Day", "Core Day", "My Data"]
     )
+    if user_name == "no":
+        user_name_input = st.subheader.text_input("Username")
+        user_password_input = st.subheader.text_input("Password")
+        if st.subheader.button("Login"):
+            user_name = user_name_input
+            user_login_True = True
+    if user_name != "no":
+        if st.subheader.button("Logout"):
+            user_name = "no"
     typeOfEx = "Home"
     resultOfChoices = ""
     if selected == "Home":
         typeOfEx = "Home"
         st.title("Welcome to the home page")
-        if user_name == "no":
-            user_name_input = st.sidebar.text_input("Username")
-            user_password_input = st.sidebar.text_input("Password")
-            if st.sidebar.button("Login"):
-                user_name = user_name_input
-                user_login_True = True
-        if user_name != "no":
-            if st.sidebar.button("Logout"):
-                user_name = "no"
-                user_login_True = False
-        
-    def userData(user_name_Data):
-        st.write(user_name_Data)
     if selected == "Chest Day":
         st.title("Lets build a chest workout")
         typeOfEx = "chest"
