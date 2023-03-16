360c199220198f423448c09d732fa6c85ad77218
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-16 18:01:33 -0500

    asd

--- GetSuperFitHome.py
+++ GetSuperFitHome.py
@@ -180,9 +180,7 @@ while varUser == False:
         user_name = ab.text_input("Usrename")
         user_password = ab.text_input("Password")
         st.form_submit_button("Login")
-        ab = st.empty()
         user_name = ab.text_input("Usrename")
-        ab = st.empty()
         user_password = ab.text_input("Password")
         st.form_submit_button("Logout")
     if user_name != "no":
