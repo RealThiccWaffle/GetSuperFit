12d180bfdb1a1de0aad4e987cc3da088840a0575
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-16 18:10:35 -0500

    sfd

--- GetSuperFitHome.py
+++ GetSuperFitHome.py
@@ -180,6 +180,9 @@ while varUser == False:
         user_name = st.text_input("Usrename")
         user_password = st.text_input("Password")
         st.form_submit_button("Login")
+        st.empty(user_name)
+        st.empty(user_password)
+        st.form_submit_button("Logout")
     if user_name != "no":
         varUser = True
 if typeOfEx == "Home":
