9b64f0b77ba51712f890fb091571af5385b94a24
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-16 17:59:26 -0500

    fhg

--- GetSuperFitHome.py
+++ GetSuperFitHome.py
@@ -181,8 +181,6 @@ while varUser == False:
         user_name = ab.text_input("Usrename")
         user_password = ab.text_input("Password")
         st.form_submit_button("Login")
-        user_name = ab.text_input("Usrename")
-        user_password = ab.text_input("Password")
         st.form_submit_button("Logout")
     if user_name != "no":
         varUser = True
