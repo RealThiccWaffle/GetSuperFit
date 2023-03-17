cc2c2c3ef0614d8d6d3965131820e317e6c64f6a
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-16 22:23:08 -0500

    sfd

--- GetSuperFitHome.py
+++ GetSuperFitHome.py
@@ -212,7 +212,7 @@ with st.sidebar.form(key='newUser'):
     if newUserCheck > 0:
         st.sidebar.text_area("Sorry, this username is taken")
     if newUserCheck < 1:
-        userListFun(userList.append(newUser))
+        userListFun(userList).append(newUser)
         st.text("it worked?")
 ###############################################################################################################
 if typeOfEx == "Home Page":
