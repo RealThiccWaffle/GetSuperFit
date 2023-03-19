3b0b960d8ec8925f88b060e3a39ca08723e61954
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-19 14:31:16 -0500

    Update GetSuperFitHome.py

--- GetSuperFitHome.py
+++ GetSuperFitHome.py
@@ -130,3 +130,38 @@ st.sidebar.write("""
 """)
 
 # ... (rest of the code remains the same)
+# ... (rest of the code remains the same)
+
+# Main workout generator code
+# ... (rest of the workout generator code)
+
+st.sidebar.title("Additional Information")
+page = st.sidebar.selectbox("Choose a topic:", ["None", "Supplements for Hypertrophy", "How to Use the App"])
+
+if page == "Supplements for Hypertrophy":
+    st.sidebar.header("Supplements for Hypertrophy")
+    st.sidebar.write("""
+    Here are some supplements that may help support muscle growth and hypertrophy:
+
+    1. **Protein powder**: Helps to meet daily protein requirements and provides the building blocks for muscle growth.
+    2. **Creatine**: Increases strength and power output, allowing for more intense workouts and faster muscle growth.
+    3. **Beta-Alanine**: Improves endurance and may help increase muscle mass in combination with resistance training.
+    4. **Branched-Chain Amino Acids (BCAAs)**: May help reduce muscle breakdown and promote recovery.
+    5. **Fish oil**: Rich in omega-3 fatty acids, which can support muscle growth and overall health.
+    """)
+
+elif page == "How to Use the App":
+    st.sidebar.header("How to Use the App")
+    st.sidebar.write("""
+    Follow these steps to generate a workout for hypertrophy:
+
+    1. Choose a training type: 'split' or 'full_body'.
+    2. If 'split' is selected, choose the target muscle group.
+    3. Choose your fitness level: 'beginner', 'intermediate', or 'advanced'.
+    4. Optionally, add extra triceps, biceps, or core exercises using the select box.
+    5. Click 'Generate Workout' to create a customized workout.
+    6. Adjust the rest time between sets using the slider.
+    7. Click 'Start Workout Timer' to begin the timer for your workout.
+    8. Perform the exercises as displayed, following the sets and reps suggested.
+    9. Prioritize proper form and technique for each exercise.
+    """)
