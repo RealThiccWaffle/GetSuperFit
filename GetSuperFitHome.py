210966e277debf7684ff410f50fce30702f3568f
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-19 22:55:35 -0500

    Update GetSuperFitHome.py

--- GetSuperFitHome.py
+++ GetSuperFitHome.py
@@ -3,7 +3,25 @@ import random
 import time
 
 def generate_workout(training_type, target_muscle, fitness_level, add_extras):
-    # ... (rest of the workout generator code)
+    workouts = {
+        # ... (workout dictionary contents)
+    }
+
+    extra_workouts = {
+        # ... (extra workouts dictionary contents)
+    }
+
+    if training_type == "full_body":
+        workout = workouts[training_type]["all"][fitness_level]
+    else:
+        workout = workouts[training_type][target_muscle][fitness_level]
+    
+    random.shuffle(workout)
+
+    if add_extras != "None":
+        workout.extend(random.sample(extra_workouts[add_extras], 2))
+
+    return workout
 
 st.title("Hypertrophy Workout Generator")
 
@@ -15,32 +33,25 @@ else:
 fitness_level = st.selectbox("Choose your fitness level:", ["beginner", "intermediate", "advanced"])
 add_extras = st.selectbox("Add extra exercises (optional):", ["None", "triceps", "biceps", "core"])
 
+# ... (rest of the code remains the same)
+
+import streamlit as st
+import random
+import time
+
+# ... (rest of the code remains the same)
+
+# Main workout generator code
+# ... (rest of the workout generator code)
+
 # Add Tone.js library
 st.markdown(
-    '<script src="https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.26/Tone.js" integrity="sha512-OoyvPgCQx/0kNNF8iNtJwvSggSAsKA16mSfY2QlySt+D4ypx/JtI3qy8fdz4pSqss5r5yD3qFoX9DTygr13rDg==" crossorigin="anonymous"></script>',
-    unsafe_allow_html=True,
+    # ... (Tone.js library import)
 )
 
 # Beep sound script
 BEEP_SOUND_SCRIPT = '''
-<script>
-async function playBeep() {
-    const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease("C5", "8n");
-}
-
-function updateTimer() {
-    window.timerId = setInterval(function() {
-        let timeRemaining = parseInt(document.getElementById('timeRemaining').innerText);
-        if (timeRemaining > 0) {
-            document.getElementById('timeRemaining').innerText = timeRemaining - 1;
-        } else {
-            clearInterval(window.timerId);
-            playBeep();
-        }
-    }, 1000);
-}
-</script>
+# ... (Beep sound script)
 '''
 
 st.markdown(BEEP_SOUND_SCRIPT, unsafe_allow_html=True)
@@ -58,62 +69,66 @@ if st.button("Start 60s Timer"):
     time_remaining.text("Time's up!")
 
 if st.button("Generate Workout"):
+    st.header("Generated Workout")
     workout = generate_workout(training_type, target_muscle, fitness_level, add_extras)
-    for exercise, sets, reps in workout:
-        st.write(f"{exercise}: {sets} sets of {reps} reps")
 
-# Sidebar content
+    for index, (exercise, sets, reps) in enumerate(workout):
+        st.subheader(f"{index + 1}. {exercise}")
+        st.write(f"Sets: {sets}")
+        st.write(f"Reps: {reps}")
+
 st.sidebar.title("Additional Information")
 page = st.sidebar.selectbox("Choose a topic:", ["None", "Hypertrophy Training Tips", "Supplements", "How to Use the App"])
-
-# ... (rest of the sidebar content)
-
 
 if page == "Hypertrophy Training Tips":
     st.sidebar.header("Hypertrophy Training Tips")
     st.sidebar.subheader("Reps and Intensity")
     st.sidebar.write("""
-    - Use a weight that is around 65-85% of your 1RM for each exercise.
-    - Perform reps at a controlled pace, e.g., 2 seconds for lifting, 1 second pause, 2 seconds for lowering.
-    - Train close to failure, but not necessarily to absolute failure on each set.
+    # ... (Hypertrophy Training Tips content)
     """)
 
     st.sidebar.subheader("Diet")
     st.sidebar.write("""
-    - Consume a calorie surplus to support muscle growth.
-    - Aim for 1.6-2.2 grams of protein per kilogram of body weight daily.
-    - Maintain a balanced intake of carbohydrates, fats, and micronutrients.
+    # ... (Diet content)
     """)
 
     st.sidebar.subheader("Split Training")
     st.sidebar.write("""
-    - Split training allows you to focus on specific muscle groups in each session.
-    - Common split routines include upper/lower, push/pull/legs, and body part-specific splits.
-    - Ensure adequate rest and recovery between sessions targeting the same muscle group.
+    # ... (Split Training content)
     """)
-elif page == "Supplements":
-    st.sidebar.header("Supplements for Hypertrophy")
-    st.sidebar.write("""
-    Here are some supplements that may help support muscle growth and hypertrophy:
+elif page == "Hypertrophy Training Tips":
+    st.title("Hypertrophy Training Tips")
+    st.write("""
+    # ... (Hypertrophy Training Tips content)
+    """)
 
-    1. **Protein powder**: Helps to meet daily protein requirements and provides the building blocks for muscle growth.
-    2. **Creatine**: Increases strength and power output, allowing for more intense workouts and faster muscle growth.
-    3. **Beta-Alanine**: Improves endurance and may help increase muscle mass in combination with resistance training.
-    4. **Branched-Chain Amino Acids (BCAAs)**: May help reduce muscle breakdown and promote recovery.
-    5. **Fish oil**: Rich in omega-3 fatty acids, which can support muscle growth and overall health.
+elif page == "Supplements":
+    st.sidebar.header("Supplements")
+    st.sidebar.subheader("Protein Powder")
+    st.sidebar.write("""
+    # ... (Protein Powder content)
     """)
+
+    st.sidebar.subheader("Creatine")
+    st.sidebar.write("""
+    # ... (Creatine content)
+    """)
+
+    st.sidebar.subheader("BCAAs")
+    st.sidebar.write("""
+    # ... (BCAAs content)
+    """)
+
 elif page == "How to Use the App":
     st.sidebar.header("How to Use the App")
     st.sidebar.write("""
-    Follow these steps to generate a workout for hypertrophy:
-
-    1. Choose a training type: 'split' or 'full_body'.
-    2. If 'split' is selected, choose the target muscle group.
-    3. Choose your fitness level: 'beginner', 'intermediate', or 'advanced'.
-    4. Optionally, add extra triceps, biceps, or core exercises using the select box.
-    5. Click 'Generate Workout' to create a customized workout.
-    6. Adjust the rest time between sets using the slider.
-    7. Click 'Start Workout Timer' to begin the timer for your workout.
-    8. Perform the exercises as displayed, following the sets and reps suggested.
-    9. Prioritize proper form and technique for each exercise.
+    1. Select the training type (split or full body) from the dropdown menu.
+    2. If you choose 'split', select the target muscle group you'd like to focus on.
+    3. Choose your fitness level (beginner, intermediate, or advanced).
+    4. Optionally, add extra exercises to your workout.
+    5. Click the 'Generate Workout' button to create your custom workout.
+    6. The generated workout will appear on the main screen with the exercises, sets, and reps.
+    7. Use the 60-second timer to time your rest periods between sets.
+    8. Explore the sidebar for additional information on hypertrophy training tips, supplements, and how to use the app.
     """)
+
