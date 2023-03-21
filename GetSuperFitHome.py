fc254108fcc68b4afe22f24c0240db3a3dae59ad
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-20 21:07:46 -0500

    Update GetSuperFitHome.py

--- GetSuperFitHome.py
+++ GetSuperFitHome.py
@@ -1,52 +1,27 @@
 import streamlit as st
 import pandas as pd
-import time
 
-# Set up the page
-st.set_page_config(page_title="Sushi pH Tracker", layout="wide")
-st.title("Sushi pH Tracker")
+def parse_date_time(date_str, time_str):
+    return f"{date_str} {time_str}"
 
-DATA_FILE = "sushi_ph_data.csv"
+st.title("Sushi Rice pH Level Tracker")
 
-def load_data():
-    try:
-        data = pd.read_csv(DATA_FILE)
-    except FileNotFoundError:
-        data = pd.DataFrame(columns=["Date", "Time", "pH"])
-        data.to_csv(DATA_FILE, index=False)
-    return data
+date_input = st.text_input("Enter date (YYYY-MM-DD):")
+time_input = st.text_input("Enter time (HH:mm):")
+ph_level = st.number_input("Enter pH level:", min_value=0.0, max_value=14.0, step=0.1)
 
-def save_data(data):
-    data.to_csv(DATA_FILE, index=False)
+add_data = st.button("Add Data")
 
-def add_ph_data(date, time, ph):
-    data = load_data()
-    new_entry = pd.DataFrame({"Date": [date], "Time": [time], "pH": [ph]})
-    data = data.append(new_entry, ignore_index=True)
-    save_data(data)
+data = {"Date_Time": [], "pH_Level": []}
 
-data = load_data()
+if add_data:
+    if date_input and time_input and ph_level:
+        date_time = parse_date_time(date_input, time_input)
+        data["Date_Time"].append(date_time)
+        data["pH_Level"].append(ph_level)
+        st.success("Data added successfully.")
+    else:
+        st.error("Please fill in all fields correctly.")
 
-# Input form
-with st.form("ph_form"):
-    st.header("Add pH Data")
-    
-    # Current date and time
-    current_time = time.localtime()
-    current_date = time.strftime("%Y-%m-%d", current_time)
-    current_time_12hour = time.strftime("%I:%M %p", current_time)
-
-    date_input = st.date_input("Date", value=pd.to_datetime(current_date))
-    time_input = st.text_input("Time", value=current_time_12hour)
-
-    ph_input = st.number_input("pH Level", min_value=0.0, max_value=14.0, step=0.01)
-
-    submit_button = st.form_submit_button("Submit")
-
-if submit_button:
-    add_ph_data(date_input, time_input, ph_input)
-    st.success("New pH data added!")
-
-# Display stored data
-st.header("Stored pH Data")
-st.write(data)
+data_df = pd.DataFrame(data)
+st.write(data_df)
