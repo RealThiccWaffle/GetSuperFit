74aa39d8e6c16bd7295667f9b3266f9b0ae31f30
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-20 19:32:50 -0500

    Update GetSuperFitHome.py

--- GetSuperFitHome.py
+++ GetSuperFitHome.py
@@ -1,20 +1,28 @@
 import streamlit as st
 import pandas as pd
-import os
 from datetime import datetime
+from session_state import get_state
 
 # Set up the page
 st.set_page_config(page_title="Sushi pH Tracker", layout="wide")
 st.title("Sushi pH Tracker")
+
 DATA_FILE = "sushi_ph_data.csv"
 
-# Check if the data file exists; if not, create one
-if not os.path.isfile(DATA_FILE):
+# Load existing data
+try:
+    data = pd.read_csv(DATA_FILE)
+except FileNotFoundError:
     data = pd.DataFrame(columns=["Date", "Time", "pH"])
     data.to_csv(DATA_FILE, index=False)
 
-# Load existing data
-data = pd.read_csv(DATA_FILE)
+# Get session state
+state = get_state()
+
+# Check if data is loaded; if not, load data
+if state.data is None:
+    state.set_data(data)
+
 # Input fields
 date_input = st.date_input("Date", value=datetime.today())
 time_input = st.time_input("Time")
@@ -27,9 +35,10 @@ ph_input = st.number_input("pH Level", min_value=0.0, max_value=14.0, step=0.01)
 # Submit button
 if st.button("Add pH Data"):
     new_entry = pd.DataFrame({"Date": [date_input], "Time": [time_12hour], "pH": [ph_input]})
-    data = data.append(new_entry, ignore_index=True)
-    data.to_csv(DATA_FILE, index=False)
+    state.data = state.data.append(new_entry, ignore_index=True)
+    state.data.to_csv(DATA_FILE, index=False)
     st.success("New pH data added!")
+
 # Display stored data
 st.header("Stored pH Data")
-st.write(data)
+st.write(state.data)
