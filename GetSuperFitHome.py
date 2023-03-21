f6c62de51a8e23b9ed261a8872257e97eba79dc0
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-20 19:56:23 -0500

    Update GetSuperFitHome.py

--- GetSuperFitHome.py
+++ GetSuperFitHome.py
@@ -16,22 +16,33 @@ def load_data():
         data.to_csv(DATA_FILE, index=False)
     return data
 
+def save_data(data):
+    data.to_csv(DATA_FILE, index=False)
+
+def add_ph_data(date, time, ph):
+    data = load_data()
+    new_entry = pd.DataFrame({"Date": [date], "Time": [time], "pH": [ph]})
+    data = data.append(new_entry, ignore_index=True)
+    save_data(data)
+
 data = load_data()
 
-# Input fields
-date_input = st.date_input("Date", value=datetime.today())
-time_input = st.time_input("Time")
+# Input form
+with st.form("ph_form"):
+    st.header("Add pH Data")
+    
+    date_input = st.date_input("Date", value=datetime.today())
+    time_input = st.time_input("Time")
 
-# Format time in 12-hour format
-time_12hour = time_input.strftime("%I:%M %p")
+    # Format time in 12-hour format
+    time_12hour = time_input.strftime("%I:%M %p")
 
-ph_input = st.number_input("pH Level", min_value=0.0, max_value=14.0, step=0.01)
+    ph_input = st.number_input("pH Level", min_value=0.0, max_value=14.0, step=0.01)
 
-# Submit button
-if st.button("Add pH Data"):
-    new_entry = pd.DataFrame({"Date": [date_input], "Time": [time_12hour], "pH": [ph_input]})
-    data = data.append(new_entry, ignore_index=True)
-    data.to_csv(DATA_FILE, index=False)
+    submit_button = st.form_submit_button("Submit")
+
+if submit_button:
+    add_ph_data(date_input, time_12hour, ph_input)
     st.success("New pH data added!")
 
 # Display stored data
