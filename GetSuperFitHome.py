2fd3687427e82a052de32c291478a8508775dd84
RealThiccWaffle <127466120+RealThiccWaffle@users.noreply.github.com>
2023-03-20 21:13:31 -0500

    Update GetSuperFitHome.py

--- GetSuperFitHome.py
+++ GetSuperFitHome.py
@@ -1,26 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-def parse_date_time(year, month, day, hour, minute, am_pm):
-    hour = int(hour)
-    if am_pm == "PM" and hour != 12:
-        hour += 12
-    elif am_pm == "AM" and hour == 12:
-        hour = 0
-    return f"{year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}"
+def parse_date_time(date_str, time_str):
+    return f"{date_str} {time_str}"
 
 st.title("Sushi Rice pH Level Tracker")
 
-with st.beta_columns(3):
-    year = st.selectbox("Year", list(range(2020, 2031)))
-    month = st.selectbox("Month", list(range(1, 13)))
-    day = st.selectbox("Day", list(range(1, 32)))
-
-with st.beta_columns(3):
-    hour = st.selectbox("Hour", list(range(1, 13)))
-    minute = st.selectbox("Minute", list(range(0, 60)))
-    am_pm = st.selectbox("AM/PM", ["AM", "PM"])
-
+date_input = st.text_input("Enter date (YYYY-MM-DD):")
+time_input = st.text_input("Enter time (HH:mm):")
 ph_level = st.number_input("Enter pH level:", min_value=0.0, max_value=14.0, step=0.1)
 
 add_data = st.button("Add Data")
@@ -28,8 +15,8 @@ add_data = st.button("Add Data")
 data = {"Date_Time": [], "pH_Level": []}
 
 if add_data:
-    if year and month and day and hour and minute and ph_level:
-        date_time = parse_date_time(year, month, day, hour, minute, am_pm)
+    if date_input and time_input and ph_level:
+        date_time = parse_date_time(date_input, time_input)
         data["Date_Time"].append(date_time)
         data["pH_Level"].append(ph_level)
         st.success("Data added successfully.")
